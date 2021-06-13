4719c69ef151d865cfa53ee451fa6fc35957d453
Android Vishnu <android@skyzon.com>
2021-06-13 12:53:02 +0530

    Before clone to bloc Development

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:unsplash_api/constants.dart';
-import 'package:unsplash_api/screenone/one.dart';
 import 'package:unsplash_api/splash.dart';
 
 
 void main() {
   runApp(MaterialApp(
-    title: 'Flutter Demo',
+    title: 'UnSplash',
     theme: ThemeData(
       primaryColor: appbarcolor,
     ),
