1f9e04343d01a1cf72e192ca7c85a45ac55b76d5
Vishnu Priyan S S <vishnu32510@gmail.com>
2024-03-17 18:35:06 -0500

    Revert "Before clone to bloc Development"
    This reverts commit 4719c69ef151d865cfa53ee451fa6fc35957d453.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:unsplash_api/constants.dart';
+import 'package:unsplash_api/screenone/one.dart';
 import 'package:unsplash_api/splash.dart';
 
 
 void main() {
   runApp(MaterialApp(
-    title: 'UnSplash',
+    title: 'Flutter Demo',
     theme: ThemeData(
       primaryColor: appbarcolor,
     ),
